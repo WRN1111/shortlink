f15b491c926679f8e1f0471a761525241d312c4e
WRN1111 <wangruoningzhenshuai@gmail.com>
2024-12-30 19:35:57 +0800

    短链接基础组件

--- project/src/main/java/org/wrn/shortlink/project/ShortLinkApplication.java
+++ project/src/main/java/org/wrn/shortlink/project/ShortLinkApplication.java
@@ -1,13 +1,14 @@
 package org.wrn.shortlink.project;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjectApplication {
+@MapperScan("org.wrn.shortlink.project.dao.mapper")
+public class ShortLinkApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ProjectApplication.class, args);
+        SpringApplication.run(ShortLinkApplication.class, args);
     }
-
 }
---
+++ project/src/main/java/org/wrn/shortlink/project/common/convention/exception/RemoteException.java
@@ -0,0 +1,27 @@
+package org.wrn.shortlink.project.common.convention.exception;
+
+/**
+ * 远程服务调用异常
+ */
+public class RemoteException extends AbstractException {
+
+    public RemoteException(String message) {
+        this(message, null, BaseErrorCode.REMOTE_ERROR);
+    }
+
+    public RemoteException(String message, IErrorCode errorCode) {
+        this(message, null, errorCode);
+    }
+
+    public RemoteException(String message, Throwable throwable, IErrorCode errorCode) {
+        super(message, throwable, errorCode);
+    }
+
+    @Override
+    public String toString() {
+        return "RemoteException{" +
+                "code='" + errorCode + "'," +
+                "message='" + errorMessage + "'" +
+                '}';
+    }
+}
