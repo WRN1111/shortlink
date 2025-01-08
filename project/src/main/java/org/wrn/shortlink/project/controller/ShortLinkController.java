42c731733404f63344eadd4df599be380d0c0800
WRN1111 <wangruoningzhenshuai@gmail.com>
2025-01-08 20:03:44 +0800

    短链接跳转界面优化

--- project/src/main/java/org/wrn/shortlink/project/controller/ShortLinkController.java
+++ project/src/main/java/org/wrn/shortlink/project/controller/ShortLinkController.java
@@ -1,6 +1,8 @@
 package org.wrn.shortlink.project.controller;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.wrn.shortlink.project.common.convention.result.Result;
@@ -9,14 +11,25 @@ import org.wrn.shortlink.project.dto.req.ShortLinkCreateReqDTO;
 import org.wrn.shortlink.project.dto.req.ShortLinkPageReqDTO;
 import org.wrn.shortlink.project.dto.req.ShortLinkUpdateReqDTO;
 import org.wrn.shortlink.project.dto.resp.ShortLinkCreateRespDTO;
+import org.wrn.shortlink.project.dto.resp.ShortLinkGroupCountQueryRespDTO;
 import org.wrn.shortlink.project.dto.resp.ShortLinkPageRespDTO;
 import org.wrn.shortlink.project.service.ShortLinkService;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class ShortLinkController {
 
     private final ShortLinkService shortLinkService;
+
+    /**
+     * 短链接跳转原始链接
+     */
+    @GetMapping("/{short-uri}")
+    public void restoreUrl(@PathVariable("short-uri") String shortUri, ServletRequest request, ServletResponse response) {
+        shortLinkService.restoreUrl(shortUri, request, response);
+    }
 
     /**
      * 创建短链接
@@ -41,5 +54,12 @@ public class ShortLinkController {
     @GetMapping("/api/short-link/v1/page")
     public Result<IPage<ShortLinkPageRespDTO>> pageShortLink(ShortLinkPageReqDTO requestParam) {
         return Results.success(shortLinkService.pageShortLink(requestParam));
+    }
+    /**
+     * 查询短链接分组内数量
+     */
+    @GetMapping("/api/short-link/v1/count")
+    public Result<List<ShortLinkGroupCountQueryRespDTO>> listGroupShortLinkCount(@RequestParam("requestParam") List<String> requestParam) {
+        return Results.success(shortLinkService.listGroupShortLinkCount(requestParam));
     }
 }
