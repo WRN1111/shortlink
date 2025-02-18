4bfb7c5d9c9f5f43102690fa3643922b1fd0f75d
WRN1111 <wangruoningzhenshuai@gmail.com>
2025-02-18 22:08:52 +0800

    短链接后台管理监控接口

---
+++ admin/src/main/java/org/wrn/shortlink/admin/controller/ShortLinkStatsController.java
@@ -0,0 +1,31 @@
+package org.wrn.shortlink.admin.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.wrn.shortlink.admin.common.convention.result.Result;
+import org.wrn.shortlink.admin.dto.req.ShortLinkStatsReqDTO;
+import org.wrn.shortlink.admin.dto.req.ShortLinkStatsRespDTO;
+import org.wrn.shortlink.admin.remote.ShortLinkRemoteService;
+
+/**
+ * 短链接监控控制层
+ */
+@RestController
+@RequiredArgsConstructor
+public class ShortLinkStatsController {
+
+    /**
+     * 后续重构为 SpringCloud Feign 调用
+     */
+    ShortLinkRemoteService shortLinkRemoteService = new ShortLinkRemoteService() {
+    };
+
+    /**
+     * 访问单个短链接指定时间内监控数据
+     */
+    @GetMapping("/api/short-link/admin/v1/stats")
+    public Result<ShortLinkStatsRespDTO> shortLinkStats(ShortLinkStatsReqDTO requestParam) {
+        return shortLinkRemoteService.oneShortLinkStats(requestParam);
+    }
+}
